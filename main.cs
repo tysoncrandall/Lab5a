2aa87ff19bb9584721afe85e5fa3e58490902eda
TysonCrandall <85883671+tysoncrandall@users.noreply.github.com>
2021-06-24 19:02:24 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,32 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    double average_speed = 0;
+    int car_num = 1;
+    int counter = 0;
+    while (counter < 7)
+    {
+        Console.WriteLine ("Enter the distance covered by car #" + car_num);
+        var distance = Console.ReadLine();
+        double distance_covered = Convert.ToDouble(distance);
+
+        Console.WriteLine ("Enter the time taken by car #" + car_num);
+        var time = Console.ReadLine();
+        double time_taken = Convert.ToDouble(time);
+
+        double car_speed = (distance_covered/time_taken);
+
+        Console.WriteLine ("The speed of this car is " + (car_speed));
+
+        car_num =  car_num+1;
+
+        average_speed = (average_speed + car_speed);
+
+        counter = counter + 1;
+    }
+  double speed = (average_speed/counter);
+  Console.WriteLine("The average speed of the cars is " + speed);
+  }
+}
